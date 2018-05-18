91a5091e32d73d644ea67acd47471ba0bc82e8d2
Steven Wu <stevenwu@tixguru.co>
2018-05-18 19:24:17 +0800

    Added NAV card and other card placeholder

--- src/routes/Performance/BasicPerformance.js
+++ src/routes/Performance/BasicPerformance.js
@@ -1,45 +1,132 @@
 import React, { Component, Fragment } from 'react';
-import { Row, Col, Card } from 'antd';
+import { Row, Col, Card, Tooltip, Icon } from 'antd';
+import { ChartCard, dollar } from 'components/Charts';
+// import numeral from 'numeral';
+// import Trend from 'components/Trend';
+import NumberInfo from 'components/NumberInfo';
+
 import Title from 'components/Title';
 
 import styles from './BasicPerformance.less';
 
+const Dollar = ({ children }) => (
+  <span
+    dangerouslySetInnerHTML={{ __html: dollar(children) }}
+  /> /* eslint-disable-line react/no-danger */
+);
+
 export default class BasicPerformance extends Component {
   render() {
+    const topColResponsiveProps = {
+      xs: 24,
+      sm: 24,
+      md: 12,
+      lg: 12,
+      xl: 12,
+      style: { marginBottom: 24 },
+    };
+
     return (
       <Fragment>
         <section className={styles.section}>
           <Title title="Net Asset Value" />
           <div>
-            <Row gutter={4}>
-              <Col span={16} push={8}>
-                <Card style={{ width: '100%' }}>
+            <Row type="flex" justify="start" gutter={4}>
+              <Col {...topColResponsiveProps}>
+                <ChartCard
+                  bordered={false}
+                  title="NAV"
+                  action={
+                    <Tooltip title="net asset value">
+                      <Icon type="info-circle-o" />
+                    </Tooltip>
+                  }
+                  total={() => <Dollar>126560</Dollar>}
+                  contentHeight={105}
+                >
+                  <span>
+                    <NumberInfo subTitle={<span>Historical</span>} status="up" subTotal={17.1} />
+                    <NumberInfo status="down" subTotal={17.1} />
+                  </span>
+                  {/* <span>
+                    <NumberInfo
+                      subTitle={<span>Historical</span>}
+                      status="up"
+                      subTotal={17.1}
+                    />
+                    <NumberInfo
+                      status="down"
+                      subTotal={17.1}
+                    />
+                  </span> */}
+                  {/* <Trend flag="up" style={{ marginRight: 16 }}>
+                    12%<span className={styles.trendText}></span>
+                  </Trend>
+                  <Trend flag="down">
+                    11%<span className={styles.trendText}></span>
+                  </Trend> */}
+                </ChartCard>
+                {/* <Card style={{ width: '100%', height: '50%', background: '#373E54', color: '#FFF' }}>
+                  <div className={styles.navContainer}>
+                    <div className={styles.nav}>
+                      <div className={styles.navInfoTitle}>NAV <span style={{ fontSize: 18 }}>(net asset value)</span></div>
+                      <div id="nav" className={styles.navInfoSubtitle}>$2.73</div>
+                    </div>
+                    <div className={styles.navHistoricalWrapper}>
+                      <h5>Historical&nbsp;</h5>
+                      <Trend flag="up" style={{ marginRight: 16 }}>
+                        周同比<span className={styles.trendText}>12%</span>
+                      </Trend>
+                      <Trend flag="down">
+                        日环比<span className={styles.trendText}>11%</span>
+                      </Trend>
+                    </div>
+                  </div>
+                  <div className={styles.navIndex}>
+                    <div className={styles.navIndexWrapper}>
+                      <h5>Hour&nbsp;</h5>
+                      <h6 id="navIndexHour"></h6>
+                      <h6 id="navIndexHourPct"></h6>
+                    </div>
+                    <div className={styles.navIndexWrapper}>
+                      <h5>Day&nbsp;</h5>
+                      <h6 id="navIndexDay"></h6>
+                      <h6 id="navIndexDayPct"></h6>
+                    </div>
+                    <div className={styles.navIndexWrapper}>
+                      <h5>Week&nbsp;</h5>
+                      <h6 id="navIndexWeek"></h6>
+                      <h6 id="navIndexWeekPct"></h6>
+                    </div>
+                    <div className={styles.navIndexWrapper}>
+                      <h5>Month&nbsp;</h5>
+                      <h6 id="navIndexMonth"></h6>
+                      <h6 id="navIndexMonthPct"></h6>
+                    </div>
+                  </div>
+                </Card> */}
+                <Card style={{ width: '100%', background: '#373E54', color: '#FFF' }}>
                   <p>Card content</p>
                   <p>Card content</p>
                   <p>Card content</p>
-                  <p>Card content</p>
-                  <p>Card content</p>
-                  <p>Card content</p>
-                  <p>Card content</p>
-                  <p>Card content</p>
-                  <p>Card content</p>
+                </Card>
+                <Card style={{ width: '100%', background: '#373E54', color: '#FFF' }}>
                   <p>Card content</p>
                   <p>Card content</p>
                   <p>Card content</p>
                 </Card>
               </Col>
-              <Col span={8} pull={16}>
-                <Card style={{ width: '100%' }}>
+              <Col {...topColResponsiveProps} order={3}>
+                <Card style={{ width: '100%', background: '#373E54', color: '#FFF' }}>
                   <p>Card content</p>
                   <p>Card content</p>
                   <p>Card content</p>
-                </Card>
-                <Card style={{ width: '100%' }}>
                   <p>Card content</p>
                   <p>Card content</p>
                   <p>Card content</p>
-                </Card>
-                <Card style={{ width: '100%' }}>
+                  <p>Card content</p>
+                  <p>Card content</p>
+                  <p>Card content</p>
                   <p>Card content</p>
                   <p>Card content</p>
                   <p>Card content</p>
@@ -47,7 +134,6 @@ export default class BasicPerformance extends Component {
               </Col>
             </Row>
           </div>
-          <Title title="Table of Holdings" />
         </section>
       </Fragment>
     );
