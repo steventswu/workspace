13dd4980e27506640825544e691c9db3bfeaa380
Shawn Chen <shawn@tixguru.co>
2018-05-23 15:17:52 +0800

    Add module import from 'src'

--- .webpackrc.js
+++ .webpackrc.js
@@ -9,6 +9,7 @@ export default {
     },
   },
   alias: {
+    src: path.resolve(__dirname, 'src/'),
     components: path.resolve(__dirname, 'src/components/'),
     assets: path.resolve(__dirname, 'src/assets/'),
   },
