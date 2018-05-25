ceae3ae3604887cdcca272013a07f4c480e30ea5
Shawn Chen <shawn@tixguru.co>
2018-05-25 13:35:55 +0800

    Update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,5 @@ VERSION=$(echo $VERSION_INFO | sed -e 's/^v//')
 docker build . --tag gcr.io/tg2018-ico/nginx-cap:$VERSION
 docker push gcr.io/tg2018-ico/nginx-cap:$VERSION
 kubectl set image deploy/nginx-cap nginx-cap=gcr.io/tg2018-ico/nginx-cap:$VERSION
+
+git push origin master --follow-tags
