6d96ae96e6ab9ae6a7f52974b9b794f2dd1aa1b8
Shawn Chen <shawn@tixguru.co>
2018-05-25 11:34:30 +0800

    Fix performance api protocol

--- src/utils/endpoint.js
+++ src/utils/endpoint.js
@@ -1,6 +1,8 @@
 const endpoint = process.env.NODE_ENV === 'development' ? '/auth' : '/api';
 
 export const perfEndpoint =
-  process.env.NODE_ENV === 'development' ? '/perf' : 'https://data.tixguru.co/cob/api';
+  process.env.NODE_ENV === 'development'
+    ? '/perf'
+    : `${window.location.protocol}//data.tixguru.co/cob/api`;
 
 export default endpoint;
