8a927bbaa3084670c7fbc7c334d7e574e86c5544
Shawn Chen <shawn@tixguru.co>
2018-06-05 11:38:06 +0800

    Try usage of ENV

--- config/conf.sh
+++ config/conf.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Preprend the upstream configuration
-(echo "upstream web { server $WEB_HOST; } upstream api { server $API_URI }" && cat /etc/nginx/conf.d/proxy.conf) > proxy.conf.new
+(echo "upstream web { server $WEB_SERVER; } upstream api { server $API_SERVER; }" && cat /etc/nginx/conf.d/proxy.conf) > proxy.conf.new
 mv proxy.conf.new /etc/nginx/conf.d/proxy.conf
 
 # Log the resulting configuration file
