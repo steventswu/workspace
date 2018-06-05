989b85835b96912041a73ebbb11d98882cbc4199
Shawn Chen <shawn@tixguru.co>
2018-06-05 11:43:47 +0800

    Clean up testing scripts

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 VERSION_INFO=$(git describe master --tags)
 VERSION=$(echo $VERSION_INFO | sed -e 's/^v//')
 
-docker build . --build-arg ENV=staging --tag gcr.io/tg2018-ico/nginx-cap:$VERSION
+docker build . --build-arg ENV=$ENV --tag gcr.io/tg2018-ico/nginx-cap:$VERSION
 docker push gcr.io/tg2018-ico/nginx-cap:$VERSION
 kubectl set image deploy/nginx-cap nginx-cap=gcr.io/tg2018-ico/nginx-cap:$VERSION
 
