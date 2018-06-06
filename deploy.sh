167182f13e6b2dcf2e3f786a23b624d27888dd40
Shawn Chen <shawn@tixguru.co>
2018-06-06 12:13:10 +0800

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ VERSION=$(echo $VERSION_INFO | sed -e 's/^v//')
 
 docker build . --build-arg ENV=$ENV --tag gcr.io/$PROJECT/nginx-cap:$VERSION
 docker push gcr.io/$PROJECT/nginx-cap:$VERSION
-kubectl set image --context=gke_$PROJECT_asia-east1-a_cluster-1 deploy/nginx-cap nginx-cap=gcr.io/$PROJECT/nginx-cap:$VERSION
+kubectl set image --context=gke_${PROJECT}_asia-east1-a_cluster-1 deploy/nginx-cap nginx-cap=gcr.io/$PROJECT/nginx-cap:$VERSION
 
 git push origin master --follow-tags
