e6bb40bc5ee09a13d9fd8207be1054667b084c3a
Shawn Chen <shawn@tixguru.co>
2018-06-06 12:02:21 +0800

    Update deploy scripts for production

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 VERSION_INFO=$(git describe master --tags)
 VERSION=$(echo $VERSION_INFO | sed -e 's/^v//')
 
-docker build . --build-arg ENV=$ENV --tag gcr.io/tg2018-ico/nginx-cap:$VERSION
-docker push gcr.io/tg2018-ico/nginx-cap:$VERSION
-kubectl set image deploy/nginx-cap nginx-cap=gcr.io/tg2018-ico/nginx-cap:$VERSION
+docker build . --build-arg ENV=$ENV --tag gcr.io/$PROJECT/nginx-cap:$VERSION
+docker push gcr.io/$PROJECT/nginx-cap:$VERSION
+kubectl set image --context=gke_$PROJECT_asia-east1-a_cluster-1 deploy/nginx-cap nginx-cap=gcr.io/$PROJECT/nginx-cap:$VERSION
 
 git push origin master --follow-tags
