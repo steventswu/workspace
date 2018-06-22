da2cf86235c499c52a95148c67e8ac4cbe9ffa0c
Shawn Chen <shawn@tixguru.co>
2018-06-22 17:07:49 +0800

    Add helper text

--- src/routes/Profile/Redeem.js
+++ src/routes/Profile/Redeem.js
@@ -3,70 +3,79 @@ import { Form, Select, InputNumber, Button } from 'antd';
 import { compose } from 'redux';
 import { connect } from 'dva';
 import { CONTRACTS, CONTRACT } from 'src/utils/contract';
+import numeral from 'numeral';
 
 import styles from './Redeem.less';
 
 const enhancer = compose(Form.create(), connect());
 
-export default enhancer(({ form: { getFieldDecorator, validateFields }, dispatch }) => (
-  <React.Fragment>
-    <h1>Redeem Application</h1>
-    <Form layout="vertical" hideRequiredMark className={styles.form}>
-      <Form.Item label="CAP">
-        {getFieldDecorator('cap', {
-          rules: [{ required: true, type: 'string', message: 'Choose CAP' }],
-        })(
-          <Select placeholder="Select CAP">
-            {CONTRACTS.map(value => (
-              <Select.Option key={value.key} value={value.key}>
-                {CONTRACT[value.key].label}
-              </Select.Option>
-            ))}
-          </Select>
-        )}
-      </Form.Item>
-      <Form.Item label="Wallet Address">
-        {getFieldDecorator('walletAddress', {
-          rules: [
-            {
-              required: true,
-              type: 'string',
-              message: 'Choose wallet address',
-            },
-          ],
-        })(
-          <Select placeholder="Select Wallet">
-            {['xxx', 'ooo'].map(value => (
-              <Select.Option key={value} value={value}>
-                {value}
-              </Select.Option>
-            ))}
-          </Select>
-        )}
-      </Form.Item>
-      <Form.Item label="Amount">
-        {getFieldDecorator('amount', {
-          rules: [
-            {
-              required: true,
-              message: 'Enter amount',
-            },
-          ],
-        })(<InputNumber min={0.1} step={0.1} />)}
-      </Form.Item>
-      <Form.Item label="">
-        <Button
-          type="primary"
-          onClick={() =>
-            validateFields((err, values) => {
-              if (err) return;
-              dispatch({ type: 'redeem', payload: values });
-            })
+export default enhancer(
+  ({ form: { getFieldValue, getFieldDecorator, validateFields }, dispatch }) => (
+    <React.Fragment>
+      <h1>Redeem Application</h1>
+      <Form layout="vertical" hideRequiredMark className={styles.form}>
+        <Form.Item label="CAP">
+          {getFieldDecorator('cap', {
+            rules: [{ required: true, type: 'string', message: 'Choose CAP' }],
+          })(
+            <Select placeholder="Select CAP">
+              {CONTRACTS.map(value => (
+                <Select.Option key={value.key} value={value.key}>
+                  {CONTRACT[value.key].label}
+                </Select.Option>
+              ))}
+            </Select>
+          )}
+        </Form.Item>
+        <Form.Item label="Wallet Address">
+          {getFieldDecorator('walletAddress', {
+            rules: [
+              {
+                required: true,
+                type: 'string',
+                message: 'Choose wallet address',
+              },
+            ],
+          })(
+            <Select placeholder="Select Wallet">
+              {['xxx', 'ooo'].map(value => (
+                <Select.Option key={value} value={value}>
+                  {value}
+                </Select.Option>
+              ))}
+            </Select>
+          )}
+        </Form.Item>
+        <Form.Item
+          label="Amount"
+          extra={
+            getFieldValue('amount') &&
+            `You will get ${numeral(getFieldValue('amount') * (1 - 0.03)).format('0.000')} ETH`
           }
         >
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
-  </React.Fragment>
-));
+          {getFieldDecorator('amount', {
+            rules: [
+              {
+                required: true,
+                message: 'Enter amount',
+              },
+            ],
+          })(<InputNumber min={0.1} step={0.1} />)}
+        </Form.Item>
+        <Form.Item label="">
+          <Button
+            type="primary"
+            onClick={() =>
+              validateFields((err, values) => {
+                if (err) return;
+                dispatch({ type: 'redeem', payload: values });
+              })
+            }
+          >
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </React.Fragment>
+  )
+);
