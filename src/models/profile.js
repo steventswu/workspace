67287b86cccbf00629047fa532582167d185e585
Shawn Chen <shawn@tixguru.co>
2018-06-26 10:51:04 +0800

    Finish client-side logic

--- src/models/profile.js
+++ src/models/profile.js
@@ -1,3 +1,6 @@
+import { notification } from 'antd';
+
+import Web3 from 'src/services/Web3';
 import { queryProfile } from 'src/services/api';
 import { formatAll } from './profile.helper';
 
@@ -7,6 +10,7 @@ export default {
   state: {
     transactions: [],
     portfolio: {},
+    walletList: [],
   },
 
   effects: {
@@ -18,6 +22,26 @@ export default {
         payload: formatAll(response),
       });
     },
+    *validateWallet({ payload }, { call, put }) {
+      try {
+        yield call(Web3.init);
+        yield call(Web3.validate);
+        const account = yield call(Web3.getAccount);
+
+        if (account === payload.walletAddress) {
+          yield put({
+            type: 'saveWallet',
+            payload: { account },
+          });
+          return;
+        }
+        notification.error({ message: 'Wallet address does not match' });
+      } catch (error) {
+        if (error instanceof TypeError) {
+          return notification.error({ message: error.message });
+        }
+      }
+    },
   },
 
   reducers: {
@@ -25,6 +49,12 @@ export default {
       return {
         ...state,
         ...payload,
+      };
+    },
+    saveWallet(state, { payload }) {
+      return {
+        ...state,
+        walletList: [payload.account, ...state.walletList],
       };
     },
   },
