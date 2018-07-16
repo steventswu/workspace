a19c0f7623d6431bb3e4bf6ebf7088d46cd33bf9
Shawn Chen <shawn@tixguru.co>
2018-07-16 17:20:28 +0800

    Cleanup unused utils

--- src/utils/authority.js
+++
@@ -1,7 +0,0 @@
-export function getAuthority() {
-  return localStorage.getItem('tixguru:authority');
-}
-
-export function setAuthority(authority) {
-  return localStorage.setItem('tixguru:authority', authority);
-}
