d3497a72ebed38fbdac17936a0fa36f7477bbcc5
Shawn Chen <shawn@tixguru.co>
2018-07-27 10:55:25 +0800

    Finish header redesign

--- src/components/GlobalHeader/index.js
+++ src/components/GlobalHeader/index.js
@@ -3,6 +3,8 @@ import { Button, Menu, Dropdown } from 'antd';
 import { Link } from 'dva/router';
 import { translate } from 'react-i18next';
 
+import styles from './index.less';
+
 const menuRoutes = ['profile', 'portfolio', 'transactions', 'redeem'];
 
 const GlobalHeader = ({ currentUser, logo, onClickLogout, onClickLogin, isLoading, t }) => {
@@ -27,30 +29,63 @@ const GlobalHeader = ({ currentUser, logo, onClickLogout, onClickLogin, isLoadin
       <Menu.Item key="logout">{t('logout')}</Menu.Item>
     </Menu>
   );
+  const menuButton = (
+    <Dropdown
+      className={styles.linksMenu}
+      trigger={['click']}
+      overlay={
+        <Menu>
+          <Menu.Item>
+            <Link style={{ padding: 15 }} to="/performance">
+              {t('performance')}
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link style={{ padding: 15 }} to="/buy">
+              {t('buy_cap')}
+            </Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link style={{ padding: 15 }} to="/faq">
+              {t('faq')}
+            </Link>
+          </Menu.Item>
+        </Menu>
+      }
+      placement="bottomCenter"
+    >
+      <span style={{ padding: 15 }}>MENU</span>
+    </Dropdown>
+  );
   return (
-    <React.Fragment>
+    <div className={styles.container}>
       <Link to="/">
-        <img src={logo} alt="logo" />
+        <img className={styles.logo} src={logo} alt="logo" />
       </Link>
-      <div style={{ float: 'right' }}>
-        <Link style={{ padding: '0 15px' }} to="/">
-          {t('home')}
-        </Link>
-        <Link style={{ padding: '0 15px' }} to="/performance">
-          {t('performance')}
-        </Link>
-        <Link style={{ padding: '0 15px' }} to="/buy">
-          {t('buy_cap')}
-        </Link>
-        {currentUser ? (
-          <Dropdown overlay={menu} placement="bottomCenter">
-            {button}
-          </Dropdown>
-        ) : (
-          button
-        )}
+      <div className={styles.linkContainer}>
+        <div className={styles.links}>
+          <Link style={{ padding: '0 15px' }} to="/performance">
+            {t('performance')}
+          </Link>
+          <Link style={{ padding: '0 15px' }} to="/buy">
+            {t('buy_cap')}
+          </Link>
+          <Link style={{ padding: '0 15px' }} to="/faq">
+            {t('faq')}
+          </Link>
+        </div>
+        <div className={styles.btn}>
+          {menuButton}
+          {currentUser ? (
+            <Dropdown overlay={menu} placement="bottomCenter">
+              {button}
+            </Dropdown>
+          ) : (
+            button
+          )}
+        </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 };
 
