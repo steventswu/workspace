81a5fcaa9edaf3dea99232e5ffa3fca9d341a4b4
Shawn Chen <shawn@tixguru.co>
2018-07-31 19:04:53 +0800

    Save work

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,26 +3,31 @@ import Container from 'src/components/Container';
 import router from 'umi/router';
 import { BUY } from 'src/routes';
 
+import { I18n } from 'react-i18next';
 import * as Section from './sections';
 import styles from './index.less';
 
 const handleAction = () => router.push(BUY);
 
 export default () => (
-  <React.Fragment>
-    <Container className={styles.main}>
-      <Section.Main onAction={handleAction} />
-    </Container>
-    <Container className={styles.features}>
-      <Section.Features />
-    </Container>
-    <Container className={styles.product}>
-      <Section.Product onAction={handleAction} />
-      <Section.Team />
-    </Container>
-    <Container className={styles.news}>
-      <Section.News />
-    </Container>
-    <Section.Action onAction={handleAction} />
-  </React.Fragment>
+  <I18n ns={['home']}>
+    {t => (
+      <React.Fragment>
+        <Container className={styles.main}>
+          <Section.Main onAction={handleAction} content={t('main', { returnObjects: true })} />
+        </Container>
+        <Container className={styles.features}>
+          <Section.Features t={t} />
+        </Container>
+        <Container className={styles.product}>
+          <Section.Product onAction={handleAction} t={t} />
+          <Section.Team t={t} />
+        </Container>
+        <Container className={styles.news}>
+          <Section.News t={t} />
+        </Container>
+        <Section.Action onAction={handleAction} t={t} />
+      </React.Fragment>
+    )}
+  </I18n>
 );
--- src/pages/home/sections/1.main.js
+++ src/pages/home/sections/1.main.js
@@ -1,60 +1,66 @@
 import React from 'react';
 import { Row, Col as Column, Slider, Button } from 'antd';
+import numeral from 'numeral';
 import ImageSet from 'src/components/ImageSet';
 import { eth } from 'src/assets/home';
+import { I18n } from 'react-i18next';
 import styles from './1.main.less';
 
-// prettier-ignore
-const content = {
-  title: 'CAP',
-  subtitle: 'Crypto Asset Platform ("CAP")',
-  desc: 'Trade with confidence on the world\'s leading crypto asset platform',
-  footer: 'Join with fund managers on managing investment portfolios of cryptocurrencies',
-  quote: {
-    content: '"CAP breaks the entry barrier for crypto investors, helping them to hold market in just one click."',
-    source: 'by Charlene Hong, former Yahoo Taiwan Managing Director',
-  },
-};
+class InvestmentSimulator extends React.PureComponent {
+  state = { amount: 0, start: '---', end: '---' };
 
+  changeAmount = value => this.setState({ amount: value / 100 * 10000 });
 
-export default ({ onAction }) => (
+  changeTenor = ([start, end]) => this.setState({ start, end });
+
+  render() {
+    const { content, earn: { percent = 0.2, amount = 200 } = {} } = this.props;
+    return (
+      <span className={styles.investment_container}>
+        <h2>{content.amount_title}</h2>
+        <Slider tipFormatter={null} onChange={this.changeAmount} />
+        <I18n ns={['common']}>
+          {t => <p>{t('cap_value', { value: numeral(this.state.amount).format('0,0') })}</p>}
+        </I18n>
+        <h2>{content.tenor_title}</h2>
+        <Slider tipFormatter={null} range onChange={this.changeTenor} />
+        <I18n ns={['common']}>{t => <p>{t('start_end', this.state)}</p>}</I18n>
+        <Row gutter={8} className={styles.return_container}>
+          <Column xs={{ span: 3 }} xl={{ span: 2 }}>
+            =
+          </Column>
+          <Column xs={{ span: 21 }} xl={{ span: 6 }}>
+            <div>
+              {numeral(percent).format('0%')}
+              <span className={styles.label}>{content.return}</span>
+            </div>
+          </Column>
+          <Column xs={{ span: 21, push: 3 }} xl={{ span: 14 }}>
+            <div>
+              {amount}
+              <ImageSet {...eth} alt="ETH" style={{ margin: 5 }} />
+              <span className={styles.label}>{content.earn}</span>
+            </div>
+          </Column>
+        </Row>
+      </span>
+    );
+  }
+}
+
+export default ({ onAction, content }) => (
   <main className={styles.main}>
     <Row className={styles.intro} gutter={48}>
       <Column xs={24} lg={{ span: 10 }} style={{ marginBottom: 24 }}>
         <div className={styles.title_container}>
           <h1>{content.title}</h1>
-          <p>{content.subtitle}</p>
-          <p>{content.desc}</p>
-          {content.footer}
+          <p>{content.description[0]}</p>
+          <p>{content.description[1]}</p>
+          {content.description[2]}
         </div>
       </Column>
       <Column xs={24} lg={{ span: 13, push: 1 }} style={{ marginBottom: 24 }}>
-        <span className={styles.investment_container}>
-          <h2>Investing Amount</h2>
-          <Slider tipFormatter={null} />
-          <p>1,000 CAP</p>
-          <h2>Tenor</h2>
-          <Slider tipFormatter={null} range />
-          <p>2017/6/15 ~ 2018/5/1</p>
-          <Row gutter={8} className={styles.return_container}>
-            <Column xs={{ span: 3 }} xl={{ span: 2 }}>
-              =
-            </Column>
-            <Column xs={{ span: 21 }} xl={{ span: 6 }}>
-              <div>
-                20%
-                <span className={styles.label}>Return</span>
-              </div>
-            </Column>
-            <Column xs={{ span: 21, push: 3 }} xl={{ span: 14 }}>
-              <div>
-                200
-                <ImageSet {...eth} alt="ETH" style={{ margin: 5 }} />
-                <span className={styles.label}>Earn</span>
-              </div>
-            </Column>
-          </Row>
-        </span>
+        <InvestmentSimulator content={content.investment_simulator} />
       </Column>
     </Row>
     <Row className={styles.start}>
