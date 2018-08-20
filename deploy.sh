d522e4b27b2a6dc02cdeed212ac042b0643f7e78
Steven Wu <stevenwu@tixguru.co>
2018-08-20 11:41:59 +0800

    Updated kubernetes image name

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 VERSION_INFO=$(git describe --tags)
 VERSION=$(echo $VERSION_INFO | sed -e 's/^v//')
 
-docker build . --build-arg ENV=$ENV --tag gcr.io/$PROJECT/nginx-cap:$VERSION
-docker push gcr.io/$PROJECT/nginx-cap:$VERSION
+docker build . --build-arg ENV=$ENV --tag gcr.io/$PROJECT/nginx-blockcast-1:$VERSION
+docker push gcr.io/$PROJECT/nginx-blockcast-1:$VERSION
 kubectl set image --context=gke_${PROJECT}_asia-east1-a_cluster-1 deploy/nginx-cap nginx-cap=gcr.io/$PROJECT/nginx-cap:$VERSION
 
 git push origin master --follow-tags
