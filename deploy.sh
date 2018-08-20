9f8e8c4ce4b746e742aa001ff60c915701e85d14
Steven Wu <stevenwu@tixguru.co>
2018-08-20 17:16:54 +0800

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ VERSION=$(echo $VERSION_INFO | sed -e 's/^v//')
 
 docker build . --build-arg ENV=$ENV --tag gcr.io/$PROJECT/nginx-blockcast-1:$VERSION
 docker push gcr.io/$PROJECT/nginx-blockcast-1:$VERSION
-kubectl set image --context=gke_${PROJECT}_asia-east1-a_cluster-1 deploy/nginx-cap nginx-cap=gcr.io/$PROJECT/nginx-cap:$VERSION
+kubectl set image --context=gke_${PROJECT}_asia-east1-a_cluster-1 deploy/nginx-blockcast-1 nginx-blockcast-1=gcr.io/$PROJECT/nginx-blockcast-1:$VERSION
 
 git push origin master --follow-tags
